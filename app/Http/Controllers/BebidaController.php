b5b57069bbf63253e50dc56fb865e39923f206f3
Antonio Magalhaes <antonio_magalhaes1@hotmail.com>
2016-05-10 20:10:53 +0100

    controlares actualização

--- app/Http/Controllers/BebidaController.php
+++ app/Http/Controllers/BebidaController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
-use Illuminate\Http\Request;
+
 
 use Illuminate\Support\Facades\Input;
 use App\Bebida;
--- app/Http/Controllers/MedicoController.php
+++ app/Http/Controllers/MedicoController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
-use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Input;
 use App\Medico;
