b41b49badde44ddc23db997ab210c8445600b1a6
Luís Teixeira <ltneiva@gmail.com>
2016-05-18 17:16:34 +0100

    Teste imagem.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,13 +5,19 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+                <div class="panel-heading">Diabetes</div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    A diabetes é uma doença crónica que tem que ser controlada. Siga os nossos conselhos para garatir
+                    qualidade de vida e para que este infortúnio não lhe traga mais problemas.
+                    <a href="{{ url('/register') }}">Registe-se</a> na nossa plataforma!!!
+                    <img src="{{ $url = asset('imgs/diabetes.jpg') }}">
                 </div>
             </div>
         </div>
     </div>
 </div>
 @endsection
+
+
+style="width:304px;height:228px;">
