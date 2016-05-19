7bb98a511273610728772f993706b6f968691bc1
Luís Teixeira <ltneiva@gmail.com>
2016-05-19 15:29:57 +0100

    Splash Page finalizada.

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,15 @@
+<section id="footer" class="dark">
+    <div class="topLine text-center">
+        <img src="{{ URL::asset('imgs/logodiabetes.png') }}" class="footerLogo center-block"/>
+    </div>
+
+    <div class="bottomLine text-center">
+
+        <h3>Métodos de apoio à decisão</h3>
+        <p>Trabalho desenvolvido no âmbito da unidade curricular por Luís Teixeira & António Magalhães.</p>
+
+        <p class="bold">All Rights Reserved. </p>
+
+        <p>© Copyright 2016.</p>
+    </div>
+</section>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container" xmlns="http://www.w3.org/1999/html">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Diabetes</div>
+                <div class="panel-heading"><h3>Diabetes --> Toma as decisões acertadas!!!</h3></div>
 
                 <div class="panel-body">
                     A diabetes é uma doença crónica que tem que ser controlada. Siga os nossos conselhos para garatir
                     qualidade de vida e para que este infortúnio não lhe traga mais problemas.
-                    <a href="{{ url('/register') }}">Registe-se</a> na nossa plataforma!!!
-                    <img src="{{ $url = asset('imgs/diabetes.jpg') }}">
+                    <a href="{{ url('/register') }}">Registe-se</a> na nossa plataforma!!!</br></br></br>
+                    <img src="{{ $url = asset('imgs/diabetes.jpg') }}" style="width: 100%;height: auto">
+                    </br></br></br>
+                    @include('layouts/footer')
                 </div>
             </div>
         </div>
     </div>
 </div>
+
 @endsection
-
-
-style="width:304px;height:228px;">
